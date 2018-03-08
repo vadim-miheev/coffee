fdc97b6438e6e63231a956774b3d839437ba46b5
Vadim Miheev <mnixav@gmail.com>
2018-03-08 16:51:37 +0300

    Final Version

--- views/profile/userCards.php
+++ views/profile/userCards.php
@@ -5,11 +5,20 @@
  * Date: 07.03.2018
  * Time: 21:51
  */
-foreach (CreateProfilePage::$userCards as $card) {
+if(!empty(CreateProfilePage::$userCards)){
+    foreach (CreateProfilePage::$userCards as $card) {
+        ?>
+        <a href="http://<?=$_SERVER['SERVER_NAME']?>/user/opencard/cardid/<?=$card['idcards']?>" id="cardId<?=$card['idcards']?>">
+            <div style="background-image: url(<?=$card['img']?>)"></div>
+        </a>
+        <?
+    }
+} else {
     ?>
-    <a href="http://<?=$_SERVER['SERVER_NAME']?>/user/opencard/cardid/<?=$card['idcards']?>" id="cardId<?=$card['idcards']?>">
-        <div style="background-image: url(<?=$card['img']?>)"></div>
-    </a>
+    <p style="text-align: center">У вас пока нету публикаций.
+        <a href="http://<?=$_SERVER['SERVER_NAME']?>/user/addcard">Добавьте</a>
+        новые публикации прямо сейчас)</p>
     <?
 }
+
 ?>
